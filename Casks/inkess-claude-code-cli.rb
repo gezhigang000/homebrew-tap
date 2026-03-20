7e0a95b9a9b43cde31fd5ac0648aa251e4f904cf
gezhigang <zhigang.ge@renlijia.com>
2026-03-20 12:54:38 +0800

    Update inkess-claude-code-cli to 0.3.4

--- Casks/inkess-claude-code-cli.rb
+++ Casks/inkess-claude-code-cli.rb
@@ -1,12 +1,12 @@
 cask "inkess-claude-code-cli" do
-  version "0.3.3"
+  version "0.3.4"
 
   if Hardware::CPU.arm?
     url "https://download.starapp.net/app-releases/Inkess%20Claude%20Code%20CLI-#{version}-arm64.dmg"
-    sha256 "094bd6ae9f406235f372eb43baecbd157ce5588434ef2853dd10302d3e8a9126"
+    sha256 "79e432a860aaee57fbe009873b13ef16b23ad560493947e44c8b8ffcd1c9b633"
   else
     url "https://download.starapp.net/app-releases/Inkess%20Claude%20Code%20CLI-#{version}.dmg"
-    sha256 "4c807fce6bca13de67b064a49b71f01a777cb958611e7bc12bca0a4489d93ff9"
+    sha256 "c49bf73bca1360f16cc7132698f5582d0d23ddde6bab7b6b27a31d183a05bcc6"
   end
 
   name "Inkess Claude Code CLI"
