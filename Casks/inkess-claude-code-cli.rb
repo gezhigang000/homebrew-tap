f3151f10987d96ac65ce8810d2891cb2b68e2b97
gezhigang <zhigang.ge@renlijia.com>
2026-04-05 07:55:56 +0800

    Update inkess-claude-code-cli to 0.4.6

--- Casks/inkess-claude-code-cli.rb
+++ Casks/inkess-claude-code-cli.rb
@@ -1,19 +1,16 @@
 cask "inkess-claude-code-cli" do
-  version "0.4.5"
+  version "0.4.6"
 
-  if Hardware::CPU.arm?
-    url "https://download.starapp.net/app-releases/Inkess%20Claude%20Code%20CLI-#{version}-arm64.dmg"
-    sha256 "8a9c438da9960697ffd910f13cf292f676e6f3298db316fc326fffade9617e6d"
-  else
-    url "https://download.starapp.net/app-releases/Inkess%20Claude%20Code%20CLI-#{version}.dmg"
-    sha256 "bfe7785e760a02a5a1ed772cba4c7b64db508666509efd752e40c592ad66cbcc"
-  end
+  url "https://download.starapp.net/app-releases/Inkess%20Claude%20Code%20CLI-#{version}-arm64.dmg"
+  sha256 "45acfc15b607c46aca94f39ff15177228096107c4fe95a1e505a42f5cb325efc"
 
   name "Inkess Claude Code CLI"
   desc "Zero-config Claude Code desktop client for Inkess users"
   homepage "https://llm.starapp.net"
 
   app "Inkess Claude Code CLI.app"
+
+  depends_on arch: :arm64
 
   zap trash: [
     "~/Library/Application Support/inkess-claude-code",
