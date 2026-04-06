1a586bcb0ec7d4b01f7e13e3bc3cd831439200a3
gezhigang <zhigang.ge@renlijia.com>
2026-04-06 19:11:50 +0800

    Update inkess-claude-code-pro to 0.3.33

--- Casks/inkess-claude-code-pro.rb
+++ Casks/inkess-claude-code-pro.rb
@@ -1,8 +1,8 @@
 cask "inkess-claude-code-pro" do
-  version "0.3.31"
+  version "0.3.33"
 
   url "https://download.starapp.net/pro-releases/Inkess%20Claude%20Code%20Pro-#{version}-arm64.dmg"
-  sha256 "8bf4b69f421ddfd853d033e9dfa550116bafd14b7cb5162af616fa1a8d1a1dbb"
+  sha256 "81f63db2ced76be48ad0792d23dbcf59e2127da259b8a41432a3bf56a0facad6"
 
   name "Inkess Claude Code Pro"
   desc "Zero-config Claude Code desktop client for Inkess users"
