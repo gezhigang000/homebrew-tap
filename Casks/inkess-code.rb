82d774324fa253af006bdecb1ca2be9a0c583334
gezhigang <zhigang.ge@renlijia.com>
2026-05-14 21:48:23 -0400

    Update inkess-code to 1.4.1

--- Casks/inkess-code.rb
+++ Casks/inkess-code.rb
@@ -1,8 +1,8 @@
 cask "inkess-code" do
-  version "1.4.0"
+  version "1.4.1"
 
   url "https://download.inkessai.com/pro-releases/InkessCode-#{version}-arm64.dmg"
-  sha256 "a4e253c5c61dcc0b26d829094a14a3b261db54d71c8d018b5d3bec25e630a70e"
+  sha256 "ac6c03de047525f870a0f1adb18fddc59fbe935e05c3717a98a154cd65da755a"
 
   name "Inkess Code"
   desc "Zero-config Claude Code & Codex desktop client for Inkess users"
